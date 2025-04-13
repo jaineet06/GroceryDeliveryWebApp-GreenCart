33c9fd865f408c10ba32dfd9a7369753f14b2a66
Shah Jaineet <146349322+jaineet06@users.noreply.github.com>
2025-04-13 17:10:46 +0530

    Delete server directory

--- server/routes/sellerRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { isSellerAuth, sellerLogin, sellerLogout } from '../controllers/sellerController'
-import authSeller from '../middlewares/authSeller'
-
-const sellerRouter = express.Router()
-
-sellerRouter.post('/login', sellerLogin)
-sellerRouter.get('/is-auth', authSeller, isSellerAuth)
-sellerRouter.get('/logout', sellerLogout)
